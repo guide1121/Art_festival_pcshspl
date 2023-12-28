1ed71d342d78dda9110c8263818972878b09d2d6
guide1121 <99393112+guide1121@users.noreply.github.com>
2023-12-28 22:41:52 +0700

    add 6-1 page

--- 6-1/scripts.js
+++ 6-1/scripts.js
@@ -1,7 +1,8 @@
 let currentIndex = 0;
 const artworks = [
     { title: 'Artwork 1', creator: 'Creator 1', imageSrc: 'artwork1.png', profile: 'profile1.png', description: 'This is a description of the artwork1' },
-    { title: 'Artwork 2', creator: 'Creator 2', imageSrc: 'artwork2.jpg', profile: 'profile2.jpg', description: 'This is a description of the artwork2' },
+    { title: 'Artwork 2', creator: 'Creator 2', imageSrc: 'artwork2.png', profile: 'profile2.jpg', description: 'This is a description of the artwork2' },
+    { title: 'Artwork 3', creator: 'Creator 3', imageSrc: 'artwork3.jpg', profile: 'profile3.jpg', description: 'This is a description of the artwork3' },
     // Add more artworks as needed
 ];
 
@@ -37,9 +38,21 @@ function updatePopupContent() {
 // Attach the showPopup function to the click event of each art piece
 document.addEventListener('DOMContentLoaded', function() {
     const artPieces = document.querySelectorAll('.art-piece');
-    artPieces.forEach(function(artPiece, index) {
+
+    artPieces.forEach(function(artPiece) {
         artPiece.addEventListener('click', function() {
+            const index = Array.from(artPieces).indexOf(artPiece);
             showPopup(index);
+        });
+
+        // Add an event listener to the image inside each art piece
+        const image = artPiece.querySelector('img');
+        image.addEventListener('load', function() {
+            const width = image.width;
+            const height = image.height;
+
+            artPiece.style.width = `${width}px`;
+            artPiece.style.height = `${height}px`;
         });
     });
 });
